2abe72ddf25b72f4d0829b70042e5e3aa575748a
Bruno Andrade <b.emanueandrade@hotmail.com>
2022-07-11 10:22:56 -0300

    Primeiro componente

---
+++ front-end/src/components/NotificationButton/index.tsx
@@ -0,0 +1,13 @@
+import icon from '../../assets/img/Vector.svg';
+import './style.css'
+
+
+function NotificationButton() {
+    return (
+        <div className="brmeta-red-btn">
+            <img src={icon} alt="Botão_Notificar"/>
+        </div>
+    )
+}
+
+export default NotificationButton;
