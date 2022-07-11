59ab5e3c9ce4aa19d0f5a6455899e69ebf540389
Bruno Andrade <b.emanueandrade@hotmail.com>
2022-07-11 09:28:17 -0300

    Projeto iniciado

---
+++ back-end/src/main/java/com/bruno/projetos/reactjava/dsmeta/brMeta/BrMetaApplication.java
@@ -0,0 +1,13 @@
+package com.bruno.projetos.reactjava.dsmeta.brMeta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrMetaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BrMetaApplication.class, args);
+	}
+
+}
---
+++ back-end/src/test/java/com/bruno/projetos/reactjava/dsmeta/brMeta/BrMetaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bruno.projetos.reactjava.dsmeta.brMeta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BrMetaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
