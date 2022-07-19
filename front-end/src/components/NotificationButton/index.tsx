52ad185703c3c2da27276bf442b5d16136682c90
Bruno Andrade <b.emanueandrade@hotmail.com>
2022-07-19 10:48:08 -0300

    Mensagem front

--- front-end/src/components/NotificationButton/index.tsx
+++ front-end/src/components/NotificationButton/index.tsx
@@ -1,10 +1,22 @@
+import axios from 'axios';
 import icon from '../../assets/img/Vector.svg';
+import { BASE_URL } from '../../utils/request';
 import './style.css'
 
+type Props = {
+    saleId: number;
+}
 
-function NotificationButton() {
+function handleClick(id: number) {
+    axios(`${BASE_URL}/sales/${id}/notification`)
+    .then(response => {
+        console.log("Mensagem enviada!")
+    })
+}
+
+function NotificationButton( {saleId}: Props) {
     return (
-        <div className="brmeta-red-btn">
+        <div className="brmeta-red-btn" onClick={() => {handleClick(saleId)}}>
             <img src={icon} alt="Botão_Notificar"/>
         </div>
     )
